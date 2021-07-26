41f628bec373e50c9b1ec50a9b0f3dcd097d5809
mitrofin <43621297+mitrofin@users.noreply.github.com>
2021-07-26 23:34:52 +0300

    routes

---
+++ routes.js
@@ -0,0 +1,68 @@
+import { lazy } from 'react';
+
+const pathes = {
+  homePage: '/',
+  loginPage: '/login',
+  registerPage: '/register',
+  contacts: '/contacts',
+  editContact: '/contacts/:contactId',
+};
+
+const HomeView = lazy(() => import('./views/HomeView/HomeView.js'));
+const ContactsView = lazy(() => import('./views/ContactsView/ContactsView.js'));
+const RegisterView = lazy(() => import('./views/RegisterView/RegisterView.js'));
+const LogInView = lazy(() => import('./views/LoginView/LoginView.js'));
+const EditContactView = lazy(() =>
+  import('./views/EditContactView/EditContactView.js'),
+);
+
+const routes = [
+  {
+    name: 'Home',
+    path: pathes.homePage,
+    exact: true,
+    component: HomeView,
+    private: false,
+    restricted: false,
+    redirectTo: pathes.homePage,
+  },
+  {
+    name: 'Contacts',
+    path: pathes.contacts,
+    exact: true,
+    component: ContactsView,
+    private: true,
+    restricted: false,
+    redirectTo: pathes.loginPage,
+  },
+  {
+    name: 'Registration',
+    path: pathes.registerPage,
+    exact: true,
+    showInMenu: true,
+    component: RegisterView,
+    private: false,
+    restricted: true,
+    redirectTo: pathes.contacts,
+  },
+  {
+    name: 'LogIn',
+    path: pathes.loginPage,
+    exact: true,
+    component: LogInView,
+    private: false,
+    restricted: true,
+    redirectTo: pathes.contacts,
+  },
+  {
+    name: 'ContactEditor',
+    path: pathes.editContact,
+    exact: true,
+    component: EditContactView,
+    private: true,
+    restricted: false,
+    redirectTo: pathes.contacts,
+  },
+];
+
+export default { pathes, routes };
